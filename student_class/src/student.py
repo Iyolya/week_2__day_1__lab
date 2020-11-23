28aa0147cc95164f8a0a40f5dda726861394a507
Laura <almasilaura@outlook.com>
2020-11-23 16:08:50 +0000

    completed test A

---
+++ student_class/src/student.py
@@ -0,0 +1,35 @@
+
+class Student:
+    pass
+
+    def __init__(self, name, cohort):    
+        self.name = name
+        self.cohort = cohort
+
+    def talk(self):
+        return "I can talk!"
+
+    def say_favourite_language(self, fav_language):
+        return (f"I love {fav_language}")    
+        
+
+        
+     
+
+student = Student("Ada", "E42")
+
+student_2 = Student("Mike", "G21")
+
+
+
+
+
+
+
+
+
+        
+
+
+
+     
